6e66691919168ae9f3e33854ab2b065ef0288f3f
Crazy Genius <elyudde@gmail.com>
2019-11-27 13:35:47 +0300

    update

--- lib/operations/settings.dart
+++ lib/operations/settings.dart
@@ -1,7 +1,9 @@
 String coopCallbackUrl = 'http://13.59.89.190:2011';
 String coopAccountNumber = '01136163949600';
 
-String peaslinkUrl = 'https://developer.co-opbank.co.ke:8280/FundsTransfer/External/A2A/PesaLink/1.0.0';
+// String peaslinkUrl = 'https://developer.co-opbank.co.ke:8243/FundsTransfer/External/A2A/PesaLink/1.0.0';
+String peaslinkUrl = 'https://openapi-sit.co-opbank.co.ke:8280/FundsTransfer/External/A2A/PesaLink/1.0.0';
+
 String coopTokenUrl = 'https://developer.co-opbank.co.ke:8243/token';
 String coopInternalFundsTransferUrl = 'https://developer.co-opbank.co.ke:8243/FundsTransfer/Internal/A2A/2.0.0';
 
