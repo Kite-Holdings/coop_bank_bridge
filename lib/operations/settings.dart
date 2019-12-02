9e28a632e99520e48502a69784cdf63539298cef
Crazy Genius <elyudde@gmail.com>
2019-12-02 15:24:14 +0300

    update

--- lib/operations/settings.dart
+++ lib/operations/settings.dart
@@ -2,7 +2,7 @@ String coopCallbackUrl = 'http://18.189.117.13:8008/';
 String coopAccountNumber = '01136163949600';
 
 // String peaslinkUrl = 'https://developer.co-opbank.co.ke:8243/FundsTransfer/External/A2A/PesaLink/1.0.0';
-String peaslinkUrl = 'https://openapi-sit.co-opbank.co.ke/FundsTransfer/External/A2A/PesaLink/1.0.0/FundsTransfer/External/A2A/PesaLink/1.0.0';
+String peaslinkUrl = 'https://openapi-sit.co-opbank.co.ke/FundsTransfer/External/A2A/PesaLink/1.0.0';
 
 String coopTokenUrl = 'https://developer.co-opbank.co.ke:8243/token';
 String coopInternalFundsTransferUrl = 'https://developer.co-opbank.co.ke:8243/FundsTransfer/Internal/A2A/2.0.0';
--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -6,7 +6,7 @@ double thirdPatyRate = 3.0;
 
 // String pesalinkTokenUrl = "https://developer.co-opbank.co.ke:8243/token";
 String pesalinkTokenUrl = "https://openapi-sit.co-opbank.co.ke:8243/token";
-String pesalinkTransactUrl = "https://openapi-sit.co-opbank.co.ke/FundsTransfer/External/A2A/PesaLink/1.0.0/FundsTransfer/External/A2A/PesaLink/1.0.0";
+String pesalinkTransactUrl = "https://openapi-sit.co-opbank.co.ke/FundsTransfer/External/A2A/PesaLink/1.0.0";
 // String pesalinkTransactUrl = "https://developer.co-opbank.co.ke:8280/FundsTransfer/External/A2A/PesaLink/1.0.0";
 String pesalinkAuth = "Basic 94dd25a9-b7e1-3906-a2f6-50525b622f35";
 Map<String, String> pesaLinkCredetials = {
